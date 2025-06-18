72ed71c439f6fb64d02d49616ae8a3735f039bcb
muneebsafdar <safdarmuneeb95@gmail.com>
2025-06-18 18:02:01 +0500

    SecondCommit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function (e) {
-  let tasksArray = JSON.parse(localStorage.getItem("Tasks")) || [];
 
+  let tasksArray = JSON.parse(localStorage.getItem("Tasks")) || [];
   fillTheTasksArea();
 
   function createWholeTaskElement(task) {
@@ -8,23 +8,17 @@ document.addEventListener("DOMContentLoaded", function (e) {
     const paragrah = document.createElement("p");
     const DeleteButton = document.createElement("button");
 
-    taskDiv.className =
-      "task w-full flex justify-between bg-gray-600 p-2 mb-2 items-center rounded";
+    taskDiv.className ="task w-full flex justify-between bg-gray-600 p-2 mb-2 items-center rounded";
     paragrah.className = "font-bold text-white";
-    DeleteButton.className =
-      "border-[1px] border-white rounded px-5 py-2 bg-gray-800 text-white cursor-pointer";
+    DeleteButton.className ="border-[1px] border-white rounded px-5 py-2 bg-gray-800 text-white cursor-pointer";
     DeleteButton.id = "delete";
-
-    
 
     paragrah.textContent = task.taskDescription;
     DeleteButton.textContent = "Delete";
     DeleteButton.setAttribute('idOfTask',String(task.id))
 
-
     DeleteButton.addEventListener("click", function (e) {
       removeTask(e.target)
-      
     });
 
     taskDiv.appendChild(paragrah);
@@ -46,9 +40,7 @@ document.addEventListener("DOMContentLoaded", function (e) {
   }
 
   function removeTask(task){
-    console.log(tasksArray.length);
     tasksArray=tasksArray.filter((t) => t.id != task.getAttribute("idOfTask"))
-    console.log(tasksArray.length);
     task.parentElement.remove()
     setStorage()
 
@@ -73,7 +65,9 @@ document.addEventListener("DOMContentLoaded", function (e) {
         .querySelector("#items_list")
         .appendChild(createWholeTaskElement(taskObject));
     }
+
     document.querySelector("#textbox").value=""
+    
   });
 });
 
